0e1608e885836696a27ea96f8a96d141aee6c5b2
Clement Gayvallet <cgayvallet@excilys.com>
2016-05-26 12:39:16 +0200

    [fix] arm64 build

--- android-jni/jni/Application.mk
+++ android-jni/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi-v7a #x86_64 # armeabi x86 x86_64 #all
+APP_ABI := armeabi-v7a arm64-v8a #x86_64 # armeabi x86 x86_64 #all
 APP_STL := c++_shared #stlport_static #gnustl_static
 #APP_OPTIM := debug		# is set by NDK_DEBUG=[1/0] command option
 LOCAL_ARM_MODE := thumb
